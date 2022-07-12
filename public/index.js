e5e662818a5883a04fa966b814159421e90deaff
Sam Bischoff <sam.bischoff2001@gmail.com>
2022-07-12 14:43:57 -0600

    jared help plz

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const { default: axios } = require("axios")
+// const { default: axios } = require("axios")
 
 const signUp = document.querySelector('#createAccount')
 const username = document.querySelector('#signupUsername')
@@ -29,7 +29,7 @@ function clearInputError(inputElement) {
 }
 
 
-document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
